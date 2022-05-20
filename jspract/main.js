b579933f349246f66784b3269dbd94d15cb9f56f
unknown <prashant808>
2022-05-20 17:23:59 +0530

    js for game with cmnts

--- jspract/main.js
+++ jspract/main.js
@@ -15,31 +15,35 @@
 //event is anything happen on click or on hover 
 // we add event listener so we can wait for certian event tpo happen 
 //it react to it
-
+//to get the random number
 let snumber = Math.trunc(Math.random() * 20 )+ 1;
 // console.log(snumber);
 
-
+// made variable for score and highscore
 let score = 20;
 let highscore =0;
-
+//event for click on check btn for to start game
 document.querySelector('.check').addEventListener
 ('click',function(){
    const guess = Number( document.querySelector('.guess').value);
    console.log(guess);
+   //to check the if guess is not input
    if(!guess){
     document.querySelector('.message').textContent = 'No number';
    }
+   //to check if number is equal to secret number
    else if(guess === snumber){
     document.querySelector('.message').textContent =  '😎 Correct number';
     document.querySelector('.number').textContent = snumber;
     document.querySelector('.container').style.backgroundColor = '#66ff33';
+    //check if score is greater than highscore then highscore is equal to new score
     if(score > highscore){
         highscore = score;
         document.querySelector('.highscore').textContent = highscore;
     }
-
+    //to check if number is greater than sec num
    }else if(guess > snumber){
+       //if no. is > sec no  then sub score by 1 
        if(score > 1){
         document.querySelector('.message').textContent = '😒 too high number';
         score--;
@@ -47,7 +51,9 @@ document.querySelector('.check').addEventListener
        }else{
         document.querySelector('.message').textContent = '☹ you lose the game ! try again';
        }
+       //to check if number is lower than sec num
    }else if(guess < snumber){
+       //if no. is > sec no  then sub score by 1 
     if(score > 1){
         document.querySelector('.message').textContent = '😒 too low number';
         score--;
@@ -58,7 +64,7 @@ document.querySelector('.check').addEventListener
    }
 }) ;
 
-//again btn 
+//again btn  added again eventlistner and resetting the values to its initial except highscore
 document.querySelector('.again').addEventListener('click', function(){
     score = 20;
     snumber = Math.trunc(Math.random() * 20 )+ 1;
